0d129632b92016a5d082ff0437d4183f2c02e4a8
VedantAdka <vedantadka@gmail.com>
2026-02-28 13:00:42 +0530

    Updated the path

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,12 @@
-import chefIcon from "../assets/Chef Claude Icon.png";
-
+import chefIcon from "./assets/Chef Claude Icon.png";
 export default function Header() {
     return (
         <header>
-            <img src={chefIcon} alt="Chef Claude Icon" />
+            <img 
+                src={chefIcon} 
+                alt="Chef Claude Icon"
+                style={{ width: "60px" }}
+            />
             <h1>Chef Claude</h1>
         </header>
     );
